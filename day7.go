97514dc3a1ad5174155283f48994661d3c08144e
Seng <engelhardt_stefan@gmx.de>
2022-11-06 08:51:42 +0100

    day 7

--- day7.go
+++ day7.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strings"
+	"regexp"
+	"strconv"
 )
 
 /*
@@ -48,25 +50,171 @@ y: 456
 
 In little Bobby's kit's instructions booklet (provided as your puzzle input), what signal is ultimately provided to wire a?
 
+--- Part Two ---
+
+Now, take the signal you got on wire a, override wire b to that signal, and reset the other wires (including wire a). What new signal is ultimately provided to wire a?
+
 */
 
-type Operation struct {
-	operands  []string
-	evaluator func([]string) int16
+// opcode
+const (
+	OpNot = iota
+	OpAssign
+	OpAnd
+	OpOr
+	OpRShift
+	OpLShift
+)
+
+// operandType
+const (
+	OperandConstant = iota
+	OperandVariable
+)
+
+type Operand struct {
+	operandType int
+	value       string
 }
 
-func parse_line(line string, operations map[string]Operation) {
-	if strings.HasPrefix(line, "NOT") {
-		line = strings.ReplaceAll(line[:2], " ", "")
-		line = strings.ReplaceAll(line, "-", "")
-		token := strings.Split(line, ">")
-		op := Operation{
-			operands: token[:0],
-			evaluator: func(ops []string) int16 {
-				return ^(operations[ops[0]].evaluator(operations[ops[0]].operands))
-			}}
-		operations[token[1]] = op
+type Expression struct {
+	lhs, rhs  Operand
+	operation int
+}
+
+func get_regex_params(r *regexp.Regexp, str string) (parameters map[string]string) {
+	match := r.FindStringSubmatch(str)
+	parameters = make(map[string]string)
+	for i, name := range r.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			parameters[name] = match[i]
+		}
 	}
+	return
+}
+
+func get_operand_type(value string) int {
+	if _, err := strconv.Atoi(value); err == nil {
+		return OperandConstant
+	} else {
+		return OperandVariable
+	}
+}
+
+func parse_line(line string, variablesMap map[string]Expression) {
+	binaryOperationRegex := regexp.MustCompile(`^(?P<operand1>[a-z0-9]+)\s+(?P<operation>[A-Z]+)\s+(?P<operand2>[a-z0-9]+)\s+(\-\>)\s+(?P<dest>[a-z0-9]+)$`)
+	notOperationRegex := regexp.MustCompile(`^(NOT)\s+(?P<operand>[a-z0-9]+)\s+(\-\>)\s+(?P<dest>[a-z0-9]+)$`)
+	assignmentRegex := regexp.MustCompile(`^(?P<operand>[a-z0-9]+)\s+(\-\>)\s+(?P<dest>[a-z0-9]+)$`)
+
+	if params := get_regex_params(binaryOperationRegex, line); len(params) > 0 {
+		var (
+			operand1, operand2 Operand
+		)
+
+		operand1.value = params["operand1"]
+		operand1.operandType = get_operand_type(operand1.value)
+
+		operand2.value = params["operand2"]
+		operand2.operandType = get_operand_type(operand2.value)
+
+		expr := Expression{lhs: operand1, rhs: operand2}
+
+		switch params["operation"] {
+		case "AND":
+			expr.operation = OpAnd
+		case "OR":
+			expr.operation = OpOr
+		case "LSHIFT":
+			expr.operation = OpLShift
+		case "RSHIFT":
+			expr.operation = OpRShift
+		default:
+			fmt.Printf("Undefined operation %v\n", params["operation"])
+		}
+
+		variablesMap[params["dest"]] = expr
+	} else if params := get_regex_params(notOperationRegex, line); len(params) > 0 {
+		operand := Operand{
+			value:       params["operand"],
+			operandType: get_operand_type(params["operand"]),
+		}
+		variablesMap[params["dest"]] = Expression{lhs: operand, operation: OpNot}
+	} else if params := get_regex_params(assignmentRegex, line); len(params) > 0 {
+		operand := Operand{
+			value:       params["operand"],
+			operandType: get_operand_type(params["operand"]),
+		}
+		variablesMap[params["dest"]] = Expression{lhs: operand, operation: OpAssign}
+	}
+}
+
+func evaluate(variable string, variablesMap map[string]Expression, evaluationCache map[string]uint16) uint16 {
+	if value, contains := evaluationCache[variable]; contains {
+		return value
+	}
+	expr := variablesMap[variable]
+
+	lhsIntValue, _ := strconv.Atoi(expr.lhs.value)
+	rhsIntValue, _ := strconv.Atoi(expr.rhs.value)
+
+	lhsExpr := func() uint16 {
+		if expr.lhs.operandType == OperandConstant {
+			return uint16(lhsIntValue)
+		} else {
+			return evaluate(expr.lhs.value, variablesMap, evaluationCache)
+		}
+	}
+	rhsExpr := func() uint16 {
+		if expr.rhs.operandType == OperandConstant {
+			return uint16(rhsIntValue)
+		} else {
+			return evaluate(expr.rhs.value, variablesMap, evaluationCache)
+		}
+	}
+
+	var returnValue uint16
+
+	switch expr.operation {
+	case OpNot:
+		returnValue = ^lhsExpr()
+	case OpAssign:
+		returnValue = lhsExpr()
+	case OpAnd:
+		returnValue = lhsExpr() & rhsExpr()
+	case OpOr:
+		returnValue = lhsExpr() | rhsExpr()
+	case OpRShift:
+		returnValue = lhsExpr() >> rhsExpr()
+	case OpLShift:
+		returnValue = lhsExpr() << rhsExpr()
+	default:
+		fmt.Printf("invald operation %v\n", expr.operation)
+		return 0
+	}
+
+	evaluationCache[variable] = returnValue
+	return returnValue
+}
+
+func day7_part1() uint16 {
+	file, err := os.Open("input/day7.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	variables := make(map[string]Expression)
+	evaluationCache := make(map[string]uint16)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parse_line(line, variables)
+	}
+
+	return evaluate("a", variables, evaluationCache)
 }
 
 func day7() (string, string) {
@@ -79,12 +227,19 @@ func day7() (string, string) {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	operands := make(map[string]Operation)
+	variables := make(map[string]Expression)
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		parse_line(line, operands)
+		parse_line(line, variables)
 	}
 
-	return "", ""
+	evaluationCache := make(map[string]uint16)
+	resultPart1 := evaluate("a", variables, evaluationCache)
+
+	evaluationCache = make(map[string]uint16)
+	evaluationCache["b"] = resultPart1
+	resultPart2 := evaluate("a", variables, evaluationCache)
+
+	return fmt.Sprint(resultPart1), fmt.Sprint(resultPart2)
 }
