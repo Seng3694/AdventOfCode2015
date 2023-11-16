4494b197b5f108e21b191380d29cf3eed39b8335
Seng <engelhardt_stefan@gmx.de>
2023-11-16 14:08:17 +0100

    refactored

--- day16/main.c
+++ day16/main.c
@@ -3,17 +3,22 @@
 #include <stdlib.h>
 #include <aoc/aoc.h>
 
+typedef enum {
+  PROP_CHILDREN,
+  PROP_SAMOYEDS,
+  PROP_AKITAS,
+  PROP_VIZLAS,
+  PROP_CARS,
+  PROP_PERFUMES,
+  PROP_CATS,
+  PROP_TREES,
+  PROP_POMERANIANS,
+  PROP_GOLDFISH,
+  PROP_COUNT,
+} property;
+
 typedef struct {
-  i16 children;
-  i16 cats;
-  i16 samoyeds;
-  i16 pomeranians;
-  i16 akitas;
-  i16 vizslas;
-  i16 goldfish;
-  i16 trees;
-  i16 cars;
-  i16 perfumes;
+  i16 properties[PROP_COUNT];
 } aunt;
 
 #define AOC_T aunt
@@ -22,47 +27,47 @@ typedef struct {
 static void parse_property(char *line, char **out, aunt *const a) {
   switch (line[0]) {
   case 'a': /* akitas */ {
-    a->akitas = (i16)strtoul(line + 8, &line, 10);
+    a->properties[PROP_AKITAS] = (i16)strtoul(line + 8, &line, 10);
     break;
   }
   case 'c': /* cars, cats, children */ {
     switch (line[2]) {
     case 'r': /* cars */ {
-      a->cars = (i16)strtoul(line + 6, &line, 10);
+      a->properties[PROP_CARS] = (i16)strtoul(line + 6, &line, 10);
       break;
     }
     case 't': /* cats */ {
-      a->cats = (i16)strtoul(line + 6, &line, 10);
+      a->properties[PROP_CATS] = (i16)strtoul(line + 6, &line, 10);
       break;
     }
     case 'i': /* children */ {
-      a->children = (i16)strtoul(line + 10, &line, 10);
+      a->properties[PROP_CHILDREN] = (i16)strtoul(line + 10, &line, 10);
       break;
     }
     }
     break;
   }
   case 'g': /* goldfish */ {
-    a->goldfish = (i16)strtoul(line + 10, &line, 10);
+    a->properties[PROP_GOLDFISH] = (i16)strtoul(line + 10, &line, 10);
     break;
   }
   case 'p': /* perfumes, pomeranians */ {
     if (line[1] == 'e')
-      a->perfumes = (i16)strtoul(line + 10, &line, 10);
+      a->properties[PROP_PERFUMES] = (i16)strtoul(line + 10, &line, 10);
     else
-      a->pomeranians = (i16)strtoul(line + 13, &line, 10);
+      a->properties[PROP_POMERANIANS] = (i16)strtoul(line + 13, &line, 10);
     break;
   }
   case 's': /* samoyeds */ {
-    a->samoyeds = (i16)strtoul(line + 10, &line, 10);
+    a->properties[PROP_SAMOYEDS] = (i16)strtoul(line + 10, &line, 10);
     break;
   }
   case 't': /* trees */ {
-    a->trees = (i16)strtoul(line + 7, &line, 10);
+    a->properties[PROP_TREES] = (i16)strtoul(line + 7, &line, 10);
     break;
   }
   case 'v': /* vizslas */ {
-    a->vizslas = (i16)strtoul(line + 9, &line, 10);
+    a->properties[PROP_VIZLAS] = (i16)strtoul(line + 9, &line, 10);
     break;
   }
   }
@@ -74,58 +79,57 @@ static void parse(char *line, void *aunts) {
   while (*line != ':')
     line++;
   line += 2;
-  aunt a = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+  aunt a = {{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1}};
   parse_property(line, &line, &a);
   parse_property(line + 1, &line, &a);
   parse_property(line + 1, &line, &a);
   aoc_vector_aunt_push(aunts, a);
 }
 
-static const aunt search = {
-    .children = 3,
-    .cats = 7,
-    .samoyeds = 2,
-    .pomeranians = 3,
-    .akitas = 0,
-    .vizslas = 0,
-    .goldfish = 5,
-    .trees = 3,
-    .cars = 2,
-    .perfumes = 1,
-};
+static const aunt search = {{3, 2, 0, 0, 2, 1, 7, 3, 3, 5}};
 
-#define PROP_SCORE(prop) (a[i].prop == search.prop || a[i].prop == -1)
-#define PROP_SCORE_GT(prop) (a[i].prop > search.prop || a[i].prop == -1)
-#define PROP_SCORE_LT(prop) (a[i].prop < search.prop || a[i].prop == -1)
-
-static u16 solve_part1(const aoc_vector_aunt *const aunts) {
-  u16 result = 0;
-  u8 maxScore = 0;
-  const aunt *const a = aunts->items;
-  for (size_t i = 0; i < aunts->length; ++i) {
-    u8 score = 0;
-    score += PROP_SCORE(children) + PROP_SCORE(cats) + PROP_SCORE(samoyeds) +
-             PROP_SCORE(pomeranians) + PROP_SCORE(akitas) +
-             PROP_SCORE(vizslas) + PROP_SCORE(goldfish) + PROP_SCORE(trees) +
-             PROP_SCORE(cars) + PROP_SCORE(perfumes);
-    if (score > maxScore) {
-      maxScore = score;
-      result = i + 1;
-    }
-  }
-  return result;
+static inline u8 prop_score(const aunt *const a, const property prop) {
+  return a->properties[prop] == search.properties[prop] ||
+         a->properties[prop] == -1;
 }
 
-static u16 solve_part2(const aoc_vector_aunt *const aunts) {
+static inline u8 prop_score_gt(const aunt *const a, const property prop) {
+  return a->properties[prop] > search.properties[prop] ||
+         a->properties[prop] == -1;
+}
+
+static inline u8 prop_score_lt(const aunt *const a, const property prop) {
+  return a->properties[prop] < search.properties[prop] ||
+         a->properties[prop] == -1;
+}
+
+typedef u8 (*score_func)(const aunt *const, const property);
+
+static const score_func part1_funcs[] = {
+    [PROP_CHILDREN] = prop_score,    [PROP_SAMOYEDS] = prop_score,
+    [PROP_AKITAS] = prop_score,      [PROP_VIZLAS] = prop_score,
+    [PROP_CARS] = prop_score,        [PROP_PERFUMES] = prop_score,
+    [PROP_CATS] = prop_score,        [PROP_TREES] = prop_score,
+    [PROP_POMERANIANS] = prop_score, [PROP_GOLDFISH] = prop_score,
+};
+
+static const score_func part2_funcs[] = {
+    [PROP_CHILDREN] = prop_score,       [PROP_SAMOYEDS] = prop_score,
+    [PROP_AKITAS] = prop_score,         [PROP_VIZLAS] = prop_score,
+    [PROP_CARS] = prop_score,           [PROP_PERFUMES] = prop_score,
+    [PROP_CATS] = prop_score_gt,        [PROP_TREES] = prop_score_gt,
+    [PROP_POMERANIANS] = prop_score_lt, [PROP_GOLDFISH] = prop_score_lt,
+};
+
+static u16 solve(const aoc_vector_aunt *const aunts,
+                 const score_func funcs[const PROP_COUNT]) {
   u16 result = 0;
   u8 maxScore = 0;
-  const aunt *const a = aunts->items;
   for (size_t i = 0; i < aunts->length; ++i) {
     u8 score = 0;
-    score += PROP_SCORE(children) + PROP_SCORE_GT(cats) + PROP_SCORE(samoyeds) +
-             PROP_SCORE_LT(pomeranians) + PROP_SCORE(akitas) +
-             PROP_SCORE(vizslas) + PROP_SCORE_LT(goldfish) +
-             PROP_SCORE_GT(trees) + PROP_SCORE(cars) + PROP_SCORE(perfumes);
+    for (property p = 0; p < PROP_COUNT; ++p)
+      score += funcs[p](&aunts->items[i], p);
+
     if (score > maxScore) {
       maxScore = score;
       result = i + 1;
@@ -139,7 +143,7 @@ int main(void) {
   aoc_vector_aunt_create(&aunts, 1 << 9);
   aoc_file_read_lines1("day16/input.txt", parse, &aunts);
 
-  printf("%u\n%u\n", solve_part1(&aunts), solve_part2(&aunts));
+  printf("%u\n%u\n", solve(&aunts, part1_funcs), solve(&aunts, part2_funcs));
 
   aoc_vector_aunt_destroy(&aunts);
 }
